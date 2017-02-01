08e996e443894625d4db45f73ba066ae9f8322ae
affanulla <as2784@njit.edu>
2017-01-31 20:18:02 -0500

    Second Commit

--- backup.sql
+++ backup.sql
@@ -37,8 +37,34 @@ CREATE TABLE `class` (
 
 LOCK TABLES `class` WRITE;
 /*!40000 ALTER TABLE `class` DISABLE KEYS */;
-INSERT INTO `class` VALUES ('DJ','000-12','UNION',9),('abcd','000-14','NEWARK',9),('Affan','111-13','newark',1);
+INSERT INTO `class` VALUES ('DJ','000-12','UNION',9),('abcd','000-14','NEWARK',9),('Affan','111-13','newark',1),('Sheriff2','1112','harrison2',20162),('Sheriff3','1114','harrison3',20163);
 /*!40000 ALTER TABLE `class` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `class2`
+--
+
+DROP TABLE IF EXISTS `class2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `class2` (
+  `student2` varchar(255) DEFAULT NULL,
+  `id2` varchar(255) NOT NULL,
+  `address2` varchar(255) DEFAULT NULL,
+  `year2` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id2`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `class2`
+--
+
+LOCK TABLES `class2` WRITE;
+/*!40000 ALTER TABLE `class2` DISABLE KEYS */;
+INSERT INTO `class2` VALUES ('Sheriff','111','harrison',2016);
+/*!40000 ALTER TABLE `class2` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -50,4 +76,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-01-25 20:14:07
+-- Dump completed on 2017-01-31 19:58:13
