9964de5046ca1fda7740754420a4c04ed725e3f5
affanulla <as2784@njit.edu>
2017-02-08 19:06:23 -0500

    Creating Cmd line interface

--- testdb.php
+++ testdb.php
@@ -1,6 +1,16 @@
 #!/usr/bin/php
 
 <?php
+
+class StudentAccess
+{
+private $db;
+funtion_contruct()
+{
+	
+}
+
+}
 $db = new mysqli("localhost","root","12345","Classes");
 
 // comment
@@ -15,7 +25,18 @@ if($db->connect_errno !=0)
 
 echo "connection successful".PHP_EOL;
 
+
+$query = "select *from class;";
+
+$queryresponse = $db -> query($query);
+var_dump($queryresponse);
+
+while($row = $queryresponse->fetch_assoc())
+{
+	print_r($row);
+}
+
 $db->close();
 
-echo "program complete\n";
+
 ?>
