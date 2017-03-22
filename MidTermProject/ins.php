76d3d5c98bfd49575fcdef969c1429dee6bb290b
affanulla <as2784@njit.edu>
2017-03-22 17:12:50 -0400

    Vehicle Insurance Management

---
+++ MidTermProject/ins.php
@@ -0,0 +1,21 @@
+<?php
+	
+/*Updating `insurableevent` table */
+	$cd = $_POST['cd'];
+	$inc_resolved = $_POST['inc_resolved'];
+	
+	$db_connection = new mysqli("localhost", "root", "12345", "test");
+	if ($db_connection->connect_error) {
+		die("Connection failed: " . $db_connection->connect_error);
+    	}
+	
+	$add_query = "UPDATE insurableevent SET resolved='$inc_resolved' WHERE incident_id='$cd';";
+	$result = $db_connection->query($add_query); 
+
+	echo "Incident info updated <br><br>";
+	echo '<form action="http://localhost/profile.html" method="post">';
+	echo '<input type="submit" value="Client Profile" name="Submit" /></form>';		
+	echo '<form action="http://localhost/index.html" method="post">';
+	echo '<input type="submit" value="Go Home" name="home" /> </form>';
+
+?>
