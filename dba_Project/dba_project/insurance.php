bdb7e8121a0e49c12026915b6325a77510cc810f
affanulla <as2784@njit.edu>
2017-03-21 22:27:19 -0400

    Holy Smokes It Works

--- dba_Project/dba_project/insurance.php
+++ dba_Project/dba_project/insurance.php
@@ -1,20 +1,28 @@
 		
 <?php
 	$id = $_POST['id'];
-	$db_connection = new mysqli("localhost", "root", "12345", "test");
+	$db_connection = new mysqli("localhost","root","12345","test");
   		if ($db_connection->connect_error) {
 	      		die("Connection failed: " . $db_connection->connect_error);
 	    	}
 		
 	$add_query = "SELECT COUNT(*), SUM(owner_fault), SUM(other_fault), SUM(cost), SUM(resolved) FROM insurableevent WHERE client_id=$id;";
-	$result = $db_connection->query($add_query); 
+	$result = $db_connection->query($add_query);
+	echo "<h4> Insurance Cost Estimation </h4>";
+	echo "Customer ID: ".$id."<br> <br>";
+	echo "<u> Incident Details </u> <br>";
+	 
 	while ($row=$result->fetch_assoc())
 	{
-		echo "\n Total number of incidents: ".$row["COUNT(*)"];
-		echo "\n Your fault: ".$row["SUM(owner_fault)"];
-		echo "\n Other party's fault: ".$row["SUM(other_fault)"];
-		echo "\n Total cost: ".$row["SUM(cost)"];
-		echo "\n Resolved: ".$row["SUM(`resolved`)"];
-	 	echo "\n Your monthly cost will be: ".($row["COUNT(*)"] + $row["SUM(cost)"]) / (12 *($row["SUM(owner_fault)"] + $row["SUM(resolved)"]));
+		
+	/*}
+ 	while($row = mysql_fetch_array($result))
+	{*/		
+		echo "&nbsp; &nbsp; Total number of incidents: ".$row['COUNT(*)']."<br>";
+		echo "&nbsp; &nbsp; Your fault: ".$row['SUM(owner_fault)']."<br>";
+		echo "&nbsp; &nbsp; Other party's fault: ".$row['SUM(other_fault)']."<br>";
+		echo "&nbsp; &nbsp; Total cost: $".$row['SUM(cost)']."<br>";
+		echo "&nbsp; &nbsp; Resolved: ".$row['SUM(resolved)']."<br><br>";
+	 	echo "Monthly Insurance cost: $".($row['COUNT(*)'] + $row['SUM(cost)']) / (12 *($row['SUM(owner_fault)'] + $row['SUM(resolved)']));
 	 }
 ?>
